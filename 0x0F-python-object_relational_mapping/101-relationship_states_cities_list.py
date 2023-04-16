dc030e6594352fdee76cd3b96992a4c23a471120
phyboro <phylisboro@gmail.com>
2023-04-16 05:23:44 -0700

    Tasks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     from sqlalchemy.orm import Session
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-            (sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
+                          (sys.argv[1], sys.argv[2], sys.argv[3]),
+                          pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     s = Session(engine)
