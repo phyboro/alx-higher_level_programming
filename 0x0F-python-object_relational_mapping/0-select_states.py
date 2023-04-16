b1639f7ac9d76c5484a478b506a98a6b96ff617d
phyboro <phylisboro@gmail.com>
2023-04-16 04:08:00 -0700

    Tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ def list_all():
     host = 'localhost'
     port = 3306
     db = MySQLdb.connect(host=host, user=username, passwd=password,
-                        db=db_name, port=port)
+            db=db_name, port=port)
     cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY id ASC;')
     result = cur.fetchall()
